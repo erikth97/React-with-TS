d02b89fbb8677dddd35cf20f442d146c4218ee69
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-09-04 12:14:22 -0600

    Feat: Add: GuitarComponent

--- guitarLA/src/App.jsx
+++ guitarLA/src/App.jsx
@@ -16,8 +16,11 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map((item, index) => (
-                <Guitar key={index} />
+            {data.map((guitar) => (
+                <Guitar 
+                    key={guitar.id}
+                    guitar={guitar}
+                />
             ))}
 
         </div>
