3e463d429e0c831918cba05627fbfbca45ba76e5
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-09-06 14:53:18 -0600

    Feat: Add: ClearCart Function

--- guitarLA/src/App.jsx
+++ guitarLA/src/App.jsx
@@ -54,6 +54,10 @@ function App() {
         setCart(updatedCart)
     }
 
+    function clearCart() {
+        setCart([])
+    }
+
   return (
     <>
     <Header 
@@ -61,6 +65,7 @@ function App() {
         removeFromCart={removeFromCart}
         decreaseQuantity={decreaseQuantity}
         increaseQuantity={increaseQuantity}
+        clearCart={clearCart}
     />
 
     <main className="container-xl mt-5">
