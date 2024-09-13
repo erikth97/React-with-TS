5258104364b472f7b915b8069fa008c7a0a35ea9
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-09-13 14:54:11 -0600

    Feat: Add: CustomHook

--- guitarLA/src/App.jsx
+++ guitarLA/src/App.jsx
@@ -1,74 +1,10 @@
-import { useEffect, useState } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
-import { db } from "./data/data"
 import { useCart } from "./hooks/useCart"
 
 function App() {
 
-    const {} = useCart()
-
-    const initialCart = () => {
-        const localStorageCart = localStorage.getItem('cart')
-        return localStorageCart ? JSON.parse(localStorageCart) : []
-    }
-
-    const [data] = useState(db)
-    const [cart, setCart] = useState(initialCart)
-
-    const MIN_ITEMS = 1
-    const MAX_ITEM = 5
-
-    useEffect(() => {
-        localStorage.setItem('cart', JSON.stringify(cart))
-    }, [cart])
-
-    function addToCart(item) {
-        const itemExsists = cart.findIndex(guitar => guitar.id === item.id)
-        if(itemExsists >= 0 ) { //exsiste en el carrito
-            if(cart[itemExsists].quantity >= MAX_ITEM) return
-            const updatedCart = [...cart]
-            updatedCart[itemExsists].quantity++
-            setCart(updatedCart)
-        } else {
-            item.quantity = 1
-            setCart([...cart, item])
-        }
-    }
-
-    function removeFromCart(id) {
-        setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
-    }
-
-    function decreaseQuantity(id) {
-        const updatedCart = cart.map( item => {
-            if(item.id === id && item.quantity > MIN_ITEMS) {
-                return {
-                    ...item,
-                    quantity: item.quantity -1
-                }
-            }
-            return item
-        })
-        setCart(updatedCart)
-    }
-
-    function increaseQuantity(id) {
-        const updatedCart = cart.map( item => {
-            if(item.id === id && item.quantity < MAX_ITEM) {
-                return {
-                    ...item,
-                    quantity: item.quantity + 1
-                }
-            }
-            return item
-        })
-        setCart(updatedCart)
-    }
-
-    function clearCart() {
-        setCart([])
-    }   
+    const {} = useCart() 
 
   return (
     <>
