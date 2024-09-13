5a9cd0607af692069e362405f5e68314296d87da
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-09-12 20:34:07 -0600

    Feat: Add: LocalStorage hook in cart

--- guitarLA/src/App.jsx
+++ guitarLA/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { db } from "./data/data"
@@ -10,6 +10,10 @@ function App() {
 
     const MIN_ITEMS = 1
     const MAX_ITEM = 5
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
 
     function addToCart(item) {
         const itemExsists = cart.findIndex(guitar => guitar.id === item.id)
@@ -56,7 +60,7 @@ function App() {
 
     function clearCart() {
         setCart([])
-    }
+    }   
 
   return (
     <>
