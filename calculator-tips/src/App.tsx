16f07b9d75749489c01e5a28c62c668198edf247
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-10-05 19:15:26 -0600

    Feat: Add: UseOrder Hook

--- calculator-tips/src/App.tsx
+++ calculator-tips/src/App.tsx
@@ -10,10 +10,10 @@ function App() {
       </header>
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div className=" p-5">
-          <h2 className=" text-4xl font-black">Menu</h2>
+        <div className='p-5'>
+          <h2 className='font-black text-4xl'>Menu</h2>
 
-          <div className=" space-y-3">
+          <div className="mt-10 space-y-3">
             {menuItems.map(item => (
               <MenuItem
                 key={item.id}
