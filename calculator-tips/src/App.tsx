f7a3d032e94ecda3f89031043e33b1151d66b2c1
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-10-05 18:26:17 -0600

    Feat: Add: Menu Item Component

--- calculator-tips/src/App.tsx
+++ calculator-tips/src/App.tsx
@@ -1,15 +1,32 @@
+import MenuItem from "./components/MenuItem"
 import { menuItems } from "./data/db"
 
-
 function App() {
-
-  console.log(menuItems)
 
   return (
     <>
       <header className=" bg-teal-400 py-5">
         <h1 className=" text-center text-4xl font-black">Propinas y Consumo App</h1>
       </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+
+          {menuItems.map(item => (
+            <MenuItem
+              key={item.id}
+              />
+          ))}
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+
+
+        </div>
+      </main>
+
     </>
   )
 }
