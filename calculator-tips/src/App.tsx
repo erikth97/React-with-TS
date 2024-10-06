fbd4378eb9190bbfb3f59f4ca5a0365ce3d374d3
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-10-05 18:52:00 -0600

    Feat: Add: Listing menu products

--- calculator-tips/src/App.tsx
+++ calculator-tips/src/App.tsx
@@ -10,14 +10,17 @@ function App() {
       </header>
 
       <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menu</h2>
+        <div className=" p-5">
+          <h2 className=" text-4xl font-black">Menu</h2>
 
-          {menuItems.map(item => (
-            <MenuItem
-              key={item.id}
-              />
-          ))}
+          <div className=" space-y-3">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                />
+            ))}
+          </div>
         </div>
 
         <div>
