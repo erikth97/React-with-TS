88802726ea6c605e18c989cdcf8a56d916914088
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-10-17 15:34:42 -0600

    Feat: Create OrderTotal Component

--- calculator-tips/src/App.tsx
+++ calculator-tips/src/App.tsx
@@ -1,11 +1,12 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
 import { menuItems } from "./data/db"
 import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  const {order, addItem} = useOrder()
+  const {order, addItem, remuveItem} = useOrder()
 
   return (
     <>
@@ -31,7 +32,10 @@ function App() {
         <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
           <OrderContents
           order={order}
+          remuveItem={remuveItem}
           />
+
+          <OrderTotals />
 
 
         </div>
